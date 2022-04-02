70a38bb53aaab3d2ad1e261f5d3b00e8a4d1fef6
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-04-01 22:11:30 -0700

    exposed types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { TimelineButtons } from './TimelineButtons'
 export { TimeMachineActive } from './TimeMachineActive'
 export { Timeline } from './Timeline'
 export { NodeHistory } from './NodeHistory'
+export { QUERY_NODE, QUERY_HIERARCHY, ObjectChangeHistory } from './NodeHistory/types'
 
 export { TimelineUtils } from './timeline-utils'
 
